9c6afaaec955260a623b0dca130886d4d367363f
takashi shinohara <git@receiver.aulta.net>
2024-06-08 04:15:49 +0900

    設定ファイル

---
+++ update_lets_encrypt/config.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * 設定ファイル
+ *
+ * @var array $config
+ */
+
+$config = [];
+
+// APIキー
+$config['value_domain_api_key'] = 'バリュードメインで発行したAPIキー';
+
+// PHP
+$config['php_command'] = 'php';
+
+// https://aulta.co.jp/technical/server-build/centos7/php/source-install-php-7-4-6
+// $config['php_command'] = '/usr/local/lib/php-7.1.6-mysqlc-mysqlnd/bin/php-7.1.6-mysqlc-mysqlnd';
+
+// ログ
+$config['log_path'] = '/var/log/update_lets_encrypt_' . date('Y') . '.log';
+
+// ドメイン
+$config['certbots'] = [];
+
+$config['certbots'][] = [
+    'execution' => true,
+    'dns' => 'value_domain',
+    'domain' => 'example.com',
+    'domains' => [
+        '*.example.com',
+        'example.com'
+    ]
+];
+
+$config['certbot_domains'][] = [
+    'execution' => true,
+    'dns' => 'value_domain',
+    'domain' => 'example.net',
+    'domains' => [
+        '*.example.net'
+    ]
+];
+
+$config['certbot_domains'][] = [
+    'execution' => true,
+    'dns' => 'value_domain',
+    'domain' => 'example.jp',
+    'domains' => [
+        'example.jp'
+    ]
+];
+
